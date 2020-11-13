9d4367efef1dbea1e547edd8f4977143d43fdb57
Carlos <carlosmiller466@gmail.com>
2020-11-13 10:30:46 -0700

    Added test_graph example

--- main.py
+++ main.py
@@ -5,3 +5,20 @@
 #  more for graphing is here.
 #
 
+import algorithims
+
+# Produce Data
+mw_production = []
+z = 2
+while True:
+    z = z*2
+    mw_production.append(z)
+    print(z)
+    if z > 1000000:
+        break
+
+# Post-processing of produced data
+time = [x for x in range(len(mw_production))]
+
+# Visualize data
+algorithims.dict_to_graph({'mW Production': mw_production, 'Time (s)': time}, 'Graph')
